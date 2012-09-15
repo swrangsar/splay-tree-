5026112d053ffa21c1cba782a6260ea3f9941ba0
Swrangsar Basumatary <swrangsar@aol.com>
2012-09-15 06:18:35 +0530

    created the zig zag functions

---
+++ splayTree.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+	void* data;
+	struct node* left;
+	struct ndoe* right;
+	struct node* parent;
+} node;
+
+typedef struct tree {
+	node* root;
+	node* min;
+	node* max;
+} tree;
+
+void zig(node* self)
+{
+	self->parent->left = self->right;
+	self->parent->parent = self;
+	self->right = self->parent;
+	self->parent = NULL;
+}
+
+void zag(node* self)
+{
+	self->parent->right = self->left;
+	self->parent->parent = self;
+	self->left = self->parent;
+	self->parent = NULL;
+}
+
+void zigzig(node* self)
+{
+	zig(self->parent);
+	zig(self);
+}
+
+void zagzag(node* self)
+{
+	zag(self->parent);
+	zag(self);
+}
+
+void zigzag(node* self)
+{
+	node* grandp = self->parent->parent;
+	zag(self);
+	self->parent = grandp;
+	grandp->left = self;
+	zig(self);
+}
+
+void zagzig(node* self)
+{
+	node* grandp = self->parent->parent;
+	zig(self);
+	self->parent = grandp;
+	grandp->right = self;
+	zag(self);
+}
+
+
+
